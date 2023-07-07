8d85de66480833dfb900d2bd2089c0c9e85b74d1
Luciano Ribeiro <luciano.ifma223@gmail.com>
2023-07-07 14:59:13 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMold() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+//pegar a tag img
+  const img = document.querySelector('#profile img')
+//substituir a imagem
+  if (html.classList.contains('light')) {
+    //se tiver com o light mode, adicionar a imagem light
+    img.setAttribute('src', './assers/avatar_light.png')
+  } else {
+    //set tiver sem o ligh mode, manter a imagem normal
+    img.setAttribute("src", "./assers/avatar.png")
+  }
+}
